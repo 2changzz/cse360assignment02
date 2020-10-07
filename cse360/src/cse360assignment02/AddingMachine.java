d35cf52b3823695631ce6f2fddb53435546a885b
sophiachang <schang70@asu.edu>
2020-10-07 15:39:49 -0700

    The original AddingMachine file with no changes to code.

---
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,52 @@
+package cse360assignment02;
+
+/**
+ * 
+ * @author sophiachang
+ *
+ */
+public class AddingMachine {
+  private int total;
+  
+  /**
+   * 
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * Returns the total
+   * @return
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * Adds values
+   * @param value
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Subtracts values
+   * @param value
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Converts the equations to strings
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Clears the AddingMachine
+   */
+  public void clear() {
+  }
+}
